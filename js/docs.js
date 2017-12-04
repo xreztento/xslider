47db711f95c7162b375d16349216c0c892976724
xreztento <xreztento@vip.sina.com>
2017-12-04 14:34:15 +0800

    version 0.0.1

---
+++ js/docs.js
@@ -0,0 +1,22 @@
+(function($){
+
+    $(document).ready(function(){
+
+        $('pre code').each(function(i, block) {
+            var plain = $(this).text();
+            $(this).parent().parent().prev().find(".btn-clipboard").click(function(){
+                if(window.clipboardData){
+                    window.clipboardData.clearData();
+                    window.clipboardData.setData("text", plain);
+                } else {
+                    var clip = new ZeroClipboard($(this));
+                    clip.setText(plain);
+                }
+            });
+
+            hljs.highlightBlock(block);
+
+        });
+    });
+
+})(jQuery);
